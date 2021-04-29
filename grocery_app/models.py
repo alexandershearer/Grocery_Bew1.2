79d42167ec76bdd84f3b3cba54087431014e78dc
alexandershearer <alex.shearer23@gmail.com>
2021-04-29 02:16:13 -0500

    Update models

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -19,6 +19,8 @@ class GroceryStore(db.Model):
     title = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(200), nullable=False)
     items = db.relationship('GroceryItem', back_populates='store')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
 
 class GroceryItem(db.Model):
     """Grocery Item model."""
@@ -27,9 +29,11 @@ class GroceryItem(db.Model):
     price = db.Column(db.Float(precision=2), nullable=False)
     category = db.Column(db.Enum(ItemCategory), default=ItemCategory.OTHER)
     photo_url = db.Column(URLType)
-    store_id = db.Column(
-        db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
+    shopping_list = db.relationship('User', secondary='shopping_list', back_populates='shopping_list_items')
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
